469524b3b7e34b609000c97128b229fb1da7f678
nemostatus <mattcorralejo1@gmail.com>
2021-04-19 09:34:07 -0700

    creating merge fn and finished mergesort recursive fn

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
-function mergeSort(array){
-    console.log(array)
-    return array
-}
+const merge = (leftArr, rightArr) =>{
+//comparison fn
+const output =[] 
+let leftIndex = 0;
+let rightIndex = 0;
+} //helper fn
+
+const mergeSort = array =>{
+    if(array.length<1){
+    //edge case for recursive fn
+    return array} //exits when this happens
+    const middleIndex = Math.floor(array.length /2)
+    const leftArr = array.slice(0, middleIndex)
+    const rightArr = array.slice(middleIndex)
+    return merge(
+        mergeSort(leftArr),
+        mergeSort(rightArr) //recursing mergeSort until theres one element
+    ) 
+    //im calling merge in this?
+} //goal of this fn break original array into indiviual arrays
 
 mergeSort([1,4,2,8,345])
 //[1,4,2,8,345]
